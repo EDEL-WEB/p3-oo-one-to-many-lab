16902c74946ce70fd3169f86bc95f054504ae07f
edel <edel.omondi@student.moringaschool.com>
2025-05-16 12:13:19 +0300

    owner is shared among many cat

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,36 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Expected a Pet instance")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
+
+        
+    
+    
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet_type: {pet_type}")
+        self.pet_type = pet_type
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("owner must be an instance of Owner or None")
+
+        self.owner = owner
+        Pet.all.append(self)
+    
